64a3433e80d0cf89c1e980696dc256d459f7899c
Jonathan-Parra1013 <jonathan-parral@unilibre.edu.co>
2025-11-06 13:43:33 -0500

    Integrantes

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template, request
-from main import obtener_equipos, obtener_jugadores 
+from main import obtener_equipos, obtener_jugadores
 
 app = Flask(__name__)
 
+# Lista de integrantes
+integrantes = [
+    "Galeano Vargas Juan Enriquen",
+    "Granja Espinosa David Santiago",
+    "Muñoz Cubides Carol Daniela",
+    "Parra Landinez Jonathan"
+]
+
+# Lista de equipos
 equipos = obtener_equipos()  # Lista de equipos para el select
 
 @app.route('/', methods=['GET', 'POST'])
@@ -11,21 +20,11 @@ def index():
     if request.method == 'POST':
         equipo_seleccionado = request.form.get('equipo')
         resultado = obtener_jugadores(equipo_seleccionado)
-    return render_template('index.html', equipos=equipos, resultado=resultado)
+    return render_template('index.html', integrantes=integrantes, equipos=equipos, resultado=resultado)
 
 if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-# INTEGRANTES 
-integrantes = ["Galeano Vargas Juan Enriquen", "Granja Espinosa David Santiago", "Muñoz Cubides Carol Daniela", "Parra Landinez Jonathan"]
-
-@app.route("/")
-def index():
-    return render_template("index.html", integrantes=integrantes)
 
