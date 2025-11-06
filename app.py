7bb8dae33cd21aa7be2ebb6045dca58ef2009803
Jonathan-Parra1013 <jonathan-parral@unilibre.edu.co>
2025-11-06 10:32:34 -0500

    app

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template, request
-from main import obtener_jugadores  # importas tu función principal
+from main import obtener_equipos, obtener_jugadores 
 
 app = Flask(__name__)
 
-equipos = obtener_jugadores.obtener_equipos()  # función que devuelve la lista de equipos
+equipos = obtener_equipos()  # Lista de equipos para el select
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     resultado = None
     if request.method == 'POST':
         equipo_seleccionado = request.form.get('equipo')
-        resultado = obtener_jugadores(jugador_equipo=equipo_seleccionado)
+        resultado = obtener_jugadores(equipo_seleccionado)
     return render_template('index.html', equipos=equipos, resultado=resultado)
 
 if __name__ == '__main__':
